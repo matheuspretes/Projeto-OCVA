6be08f1a3cd755a0ea7adced91c8dfd3b7438c21
matheuspretes <arthuroliver423@gmail.com>
2026-08-19 08:01:01 -0300

    aaaaa

--- OCVA Front-end/src/app/services/codigo-acesso-service.ts
+++ OCVA Front-end/src/app/services/codigo-acesso-service.ts
@@ -8,8 +8,7 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root',
 })
 export class CodigoAcessoService {
-  // private readonly API_URL = 'https://ocva-api.onrender.com/api/v1/codigos-acesso';
-	private readonly API_URL = 'http://localhost:8080/api/v1/codigo-acesso';
+  private readonly API_URL = 'https://ocva-api.onrender.com/api/v1/codigos-acesso';
 
   constructor(private http: HttpClient) {}
 
